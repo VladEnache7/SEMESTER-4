67c679906cfb97b1ed79c9202db2046afe96ad00
Vlad Enache <ioan.vlad.enache@stud.ubbcluj.ro>
2024-05-08 16:04:15 +0300

    DBMSs bonus project

---
+++ DBMS/DBMS_Homeworks/Homework2/Homework2/SimulateSQLInjectionAtacks_2.Designer.cs
@@ -0,0 +1,151 @@
+﻿using System.ComponentModel;
+
+namespace Homework2
+{
+    partial class SimulateSQLInjectionAtacks_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetEmployees = new System.Windows.Forms.Button();
+            this.txtBusinessRegNumber = new System.Windows.Forms.TextBox();
+            this.txtPartnersCompaniesID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetEmployees
+            // 
+            this.btnGetEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetEmployees.Location = new System.Drawing.Point(39, 300);
+            this.btnGetEmployees.Name = "btnGetEmployees";
+            this.btnGetEmployees.Size = new System.Drawing.Size(214, 40);
+            this.btnGetEmployees.TabIndex = 0;
+            this.btnGetEmployees.Text = "Get Employees";
+            this.btnGetEmployees.UseVisualStyleBackColor = true;
+            this.btnGetEmployees.Click += new System.EventHandler(this.btnGetEmployees_Click);
+            // 
+            // txtBusinessRegNumber
+            // 
+            this.txtBusinessRegNumber.Location = new System.Drawing.Point(39, 157);
+            this.txtBusinessRegNumber.Name = "txtBusinessRegNumber";
+            this.txtBusinessRegNumber.Size = new System.Drawing.Size(214, 26);
+            this.txtBusinessRegNumber.TabIndex = 1;
+            // 
+            // txtPartnersCompaniesID
+            // 
+            this.txtPartnersCompaniesID.Location = new System.Drawing.Point(39, 249);
+            this.txtPartnersCompaniesID.Name = "txtPartnersCompaniesID";
+            this.txtPartnersCompaniesID.Size = new System.Drawing.Size(214, 26);
+            this.txtPartnersCompaniesID.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Partners_CompaniesID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(308, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Business Registration Number:";
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(349, 46);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 62;
+            this.dgvEmployees.RowTemplate.Height = 28;
+            this.dgvEmployees.Size = new System.Drawing.Size(459, 391);
+            this.dgvEmployees.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(52, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Company Name:";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(39, 64);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(214, 26);
+            this.txtCompanyName.TabIndex = 7;
+            // 
+            // SimulateSQLInjectionAtacks_2
+            // 
+            this.ClientSize = new System.Drawing.Size(833, 531);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvEmployees);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPartnersCompaniesID);
+            this.Controls.Add(this.txtBusinessRegNumber);
+            this.Controls.Add(this.btnGetEmployees);
+            this.Name = "SimulateSQLInjectionAtacks_2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxBRN;
+        private System.Windows.Forms.TextBox textBoxPCID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetEmployees;
+        private System.Windows.Forms.TextBox txtBusinessRegNumber;
+        private System.Windows.Forms.TextBox txtPartnersCompaniesID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCompanyName;
+    }
+}
---
+++ DBMS/DBMS_Homeworks/Homework2/Homework2/SimulateSQLInjectionAtacks_2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Homework2
+{
+    public partial class SimulateSQLInjectionAtacks_2 : Form
+    {
+        public SimulateSQLInjectionAtacks_2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetEmployees_Click(object sender, EventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                // first check if the company with the given BusinessRegNumber and Partners_CompaniesID exists
+                var query1 =
+                    "SELECT Partners_CompaniesID FROM Partners_Companies WHERE BusinessRegNumber = @BusinessRegNumber AND Company_Name = '@CompanyName' ";
+                using (var command = new SqlCommand(query1, connection))
+                {
+                    command.Parameters.Add("@BusinessRegNumber", SqlDbType.NVarChar).Value = txtBusinessRegNumber.Text;
+                    command.Parameters.Add("@CompanyName", SqlDbType.NVarChar).Value = txtCompanyName.Text;
+
+                    connection.Open();
+                    // get the array of Partners_CompaniesID
+                    var Partners_CompaniesIDs = command.ExecuteScalar() as Array;
+
+                    if (Partners_CompaniesIDs != null && Partners_CompaniesIDs.Length > 0)
+                        // for each company found, get the employees
+                        for (var i = 0; i < Partners_CompaniesIDs.Length; i++)
+                        {
+                            var query2 =
+                                "SELECT * FROM Employees WHERE Partners_CompaniesID = @Partners_CompaniesID";
+                            using (var command2 = new SqlCommand(query2, connection))
+                            {
+                                command2.Parameters.Add("@Partners_CompaniesID", SqlDbType.NVarChar).Value =
+                                    Partners_CompaniesIDs[i];
+
+                                var adapter = new SqlDataAdapter(command2);
+                                var table = new DataTable();
+                                adapter.Fill(table);
+
+                                dgvEmployees.DataSource = table;
+                            }
+                        }
+                    else
+                        // Company does not exist, you can stop here and inform the user
+                        MessageBox.Show("No company found with the given BusinessRegNumber and Partners_CompaniesID");
+                }
+            }
+        }
+    }
+}
